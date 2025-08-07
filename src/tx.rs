e1bf0b9a7eba3c0687c34d44327d6fc6731302f5
Steven Roose <steven@stevenroose.org>
2025-08-07 12:23:29 +0100

    bitcoin funcs

--- src/tx.rs
+++ src/tx.rs
@@ -1,6 +1,9 @@
-use bitcoin::{opcodes, taproot, ScriptBuf};
-use bitcoin::secp256k1::{self, PublicKey, XOnlyPublicKey};
+use bitcoin::{
+	absolute, opcodes, sighash, taproot, Address, Amount, CompressedPublicKey, Network, OutPoint, ScriptBuf, Sequence, Transaction, TxIn, TxOut, Witness
+};
+use bitcoin::secp256k1::{self, Keypair, Message, PublicKey, XOnlyPublicKey};
 
+use crate::musig;
 
 lazy_static::lazy_static! {
 	/// Global secp context.
@@ -11,21 +14,90 @@ lazy_static::lazy_static! {
 pub const EXPIRY: usize = 144*30;
 
 
-// pub fn delayed_sign(delay_blocks: usize, pubkey: XOnlyPublicKey) -> ScriptBuf {
-// 	let csv = bitcoin::Sequence::from_height(delay_blocks as u16);
-// 	bitcoin::Script::builder()
-// 		.push_int(csv.to_consensus_u32() as i64)
-// 		.push_opcode(opcodes::all::OP_CSV)
-// 		.push_opcode(opcodes::all::OP_DROP)
-// 		.push_x_only_key(&pubkey)
-// 		.push_opcode(opcodes::all::OP_CHECKSIG)
-// 		.into_script()
-// }
-//
-// pub fn create_address(server: PublicKey, user: PublicKey) {
-// 	let agg_pk = musig::combine_keys(keys)
-// 	taproot::TaprootBuilder::new()
-// 		.add_leaf(0, delayed_sign(EXPIRY, user)).unwrap()
-// 		.finalize(&SECP, agg_pk).unwrap()
-//
-// }
+pub fn delayed_sign(delay_blocks: usize, pubkey: XOnlyPublicKey) -> ScriptBuf {
+	let csv = bitcoin::Sequence::from_height(delay_blocks as u16);
+	bitcoin::Script::builder()
+		.push_int(csv.to_consensus_u32() as i64)
+		.push_opcode(opcodes::all::OP_CSV)
+		.push_opcode(opcodes::all::OP_DROP)
+		.push_x_only_key(&pubkey)
+		.push_opcode(opcodes::all::OP_CHECKSIG)
+		.into_script()
+}
+
+
+pub fn multisig(server: PublicKey, user: PublicKey) -> ScriptBuf {
+	bitcoin::Script::builder()
+		.push_x_only_key(&server.into())
+		.push_opcode(opcodes::all::OP_CHECKSIG)
+		.push_x_only_key(&user.into())
+		.push_opcode(opcodes::all::OP_CHECKSIG)
+		.into_script()
+}
+
+
+
+pub fn create_address(server: PublicKey, user: PublicKey) -> Address {
+	let agg_pk = musig::combine_keys([user, server]);
+	let taproot = taproot::TaprootBuilder::new()
+		.add_leaf(0, delayed_sign(EXPIRY, user.into())).unwrap()
+		.add_leaf(1, multisig(server, user)).unwrap()
+		.finalize(&SECP, agg_pk).unwrap();
+
+	Address::p2tr_tweaked(taproot.output_key(), Network::Regtest)
+}
+
+pub fn update(
+	user: Keypair,
+	utxo: OutPoint,
+	server_amount: Amount,
+	user_amount: Amount,
+	server: PublicKey,
+	channel: &Transaction,
+) -> Transaction {
+	let mut tx = Transaction {
+		version: bitcoin::transaction::Version::TWO,
+		lock_time: absolute::LockTime::ZERO,
+		input: vec![
+			TxIn {
+				previous_output: utxo,
+				script_sig: ScriptBuf::new(),
+				sequence: Sequence::ZERO,
+				witness: {
+					Witness::new()
+				},
+			},
+		],
+		output: vec![
+			// server
+			TxOut {
+				value: server_amount,
+				script_pubkey: ScriptBuf::new_p2pkh(&CompressedPublicKey(server).pubkey_hash()),
+			},
+			// server
+			TxOut {
+				value: user_amount,
+				script_pubkey: ScriptBuf::new_p2pkh(&CompressedPublicKey(user.public_key()).pubkey_hash()),
+			},
+		],
+	};
+
+	let mut shc = sighash::SighashCache::new(&tx);
+
+	let leaf_hash = taproot::TapLeafHash::from_script(
+		&multisig(server, user.public_key()),
+		taproot::LeafVersion::TapScript,
+	);
+	let sighash = shc.taproot_script_spend_signature_hash(
+		0,
+		&sighash::Prevouts::All(&[&channel.output[0]]),
+		leaf_hash,
+		bitcoin::TapSighashType::Default,
+	).unwrap();
+
+	let sig = SECP.sign_schnorr(&Message::from_digest_slice(&sighash[..]).unwrap(), &user);
+
+	tx.input[0].witness.push(&sig[..]);
+
+	tx
+}
