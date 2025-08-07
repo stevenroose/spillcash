e18756c88fd81720c924a57138041081b4a8c4b8
michael1011 <me@michael1011.at>
2025-08-07 14:43:43 +0300

    semi working api

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,11 @@ mod musig;
 mod tx;
 mod web;
 
-lazy_static::lazy_static! {
-    static ref SERVER_PK: PublicKey = PublicKey::from_str("030c99c81e19622d30cc5fe697f65f03eed93bbf177df99065a2d1f3141dcd095e").unwrap();
-}
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // Initialize cashu operations
     mint(None).await?;
     println!("SpillCash initialized!");
-
-    let user_key =
-        Keypair::from_str("b90c5fa5e7c920f2582d67a440a3f2b1c09fff588f42b0a04754f1e7fb63a2d2")
-            .unwrap();
-    let fund_address = tx::create_address(*SERVER_PK, user_key.public_key());
-
-    // now send to address
 
     // Create and start the web server
     let app = web::create_app();
--- src/web.rs
+++ src/web.rs
@@ -1,3 +1,4 @@
+use crate::tx;
 use axum::{
     Router,
     extract::Path,
@@ -5,29 +6,40 @@ use axum::{
     response::Json,
     routing::{get, post},
 };
+use bitcoin::secp256k1::{Keypair, PublicKey};
+use bitcoin::{Transaction, consensus::deserialize};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::collections::HashMap;
-use std::sync::Arc;
+use std::str::FromStr;
+use std::sync::{Arc, Mutex};
 use tower_http::cors::CorsLayer;
 
 use crate::cashu;
 
-#[derive(Clone)]
-struct AppState {}
+lazy_static::lazy_static! {
+    static ref SERVER_PK: PublicKey = PublicKey::from_str("030c99c81e19622d30cc5fe697f65f03eed93bbf177df99065a2d1f3141dcd095e").unwrap();
+}
+
+lazy_static::lazy_static! {
+    static ref TX: Mutex<Option<Transaction>> = Mutex::new(None);
+}
 
 pub fn create_app() -> Router {
     Router::new()
         .route("/address", get(get_address))
         .route("/tx", post(submit_tx))
         .route("/token", post(get_token))
-        .with_state(Arc::new(AppState {}))
         .layer(CorsLayer::permissive())
 }
 
 async fn get_address() -> Json<serde_json::Value> {
+    let user_key =
+        Keypair::from_str("b90c5fa5e7c920f2582d67a440a3f2b1c09fff588f42b0a04754f1e7fb63a2d2")
+            .unwrap();
+
     Json(json!({
-        "address": "bc1",
+        "address": tx::create_address(*SERVER_PK, user_key.public_key()).to_string(),
     }))
 }
 
@@ -37,6 +49,10 @@ struct TxRequest {
 }
 
 async fn submit_tx(Json(payload): Json<TxRequest>) -> Json<serde_json::Value> {
+    let parsed: Transaction = deserialize(&hex::decode(payload.tx).unwrap()).unwrap();
+    let mut tx = TX.lock().unwrap();
+    *tx = Some(parsed);
+
     Json(json!({
         "status": "success",
     }))
